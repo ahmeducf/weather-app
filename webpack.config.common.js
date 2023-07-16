74136d2502e11318577f5c4eddb8f83647db402e
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-07-16 22:44:14 +0300

    build: Add webpack common configuration

---
+++ webpack.config.common.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
