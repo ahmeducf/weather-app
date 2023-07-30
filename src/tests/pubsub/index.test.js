8f1c0035de0e5112b9e19840b787bee60b20c009
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-07-31 02:44:53 +0300

    test: Add pubsub module unit tests

---
+++ src/tests/pubsub/index.test.js
@@ -0,0 +1,38 @@
+import pubsub from '../../pubsub';
+
+let mockFn;
+
+beforeEach(() => {
+  mockFn = jest.fn();
+});
+
+describe('pubsub', () => {
+  it('should notify subscribers when an event is published', () => {
+    pubsub.subscribe('test', mockFn);
+    pubsub.publish('test', 'test');
+    expect(mockFn).toHaveBeenCalledWith('test');
+  });
+
+  it('should not notify subscribers when an event is not published', () => {
+    pubsub.subscribe('test', mockFn);
+    pubsub.publish('test2', 'test');
+    expect(mockFn).not.toHaveBeenCalled();
+  });
+
+  it('should not notify subscribers when an event is unsubscribed', () => {
+    pubsub.subscribe('test', mockFn);
+    pubsub.unSubscribe('test', mockFn);
+    pubsub.publish('test', 'test');
+    expect(mockFn).not.toHaveBeenCalled();
+  });
+
+  it('it should push a new event to the events object', () => {
+    mockFn = jest.fn((event) => event);
+
+    pubsub.subscribe('test', mockFn);
+    pubsub.publish('test', 'test');
+
+    expect(mockFn).toHaveBeenCalledWith('test');
+    expect(mockFn).toHaveReturnedWith('test');
+  });
+});
