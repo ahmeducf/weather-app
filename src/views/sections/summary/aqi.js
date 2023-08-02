bfc0c2ecbe9b34f84943bde2b7d0cdb3ba2bf1e6
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-08-02 05:59:35 +0300

    fix: Air quality index donut chart indicator

--- src/views/sections/summary/aqi.js
+++ src/views/sections/summary/aqi.js
@@ -22,26 +22,70 @@ const renderAqiSummary = (app) => {
 
   if (airQualityIndex <= 50) {
     donutChartSVG.classList.add('good');
+    donutChartSVG.classList.remove(
+      'moderate',
+      'unhealthy-for-sensitive-groups',
+      'unhealthy',
+      'very-unhealthy',
+      'hazardous',
+    );
+
     donutChartIndicator.style.strokeDasharray =
       '28.797932657906433 172.78759594743863';
   } else if (airQualityIndex <= 100) {
     donutChartSVG.classList.add('moderate');
+    donutChartSVG.classList.remove(
+      'good',
+      'unhealthy-for-sensitive-groups',
+      'unhealthy',
+      'very-unhealthy',
+      'hazardous',
+    );
+
     donutChartIndicator.style.strokeDasharray =
       '57.595865315812866 172.78759594743863';
   } else if (airQualityIndex <= 150) {
     donutChartSVG.classList.add('unhealthy-for-sensitive-groups');
+    donutChartSVG.classList.remove(
+      'good',
+      'moderate',
+      'unhealthy',
+      'very-unhealthy',
+      'hazardous',
+    );
     donutChartIndicator.style.strokeDasharray =
       '86.39379797371932 172.78759594743863';
   } else if (airQualityIndex <= 200) {
     donutChartSVG.classList.add('unhealthy');
+    donutChartSVG.classList.remove(
+      'good',
+      'moderate',
+      'unhealthy-for-sensitive-groups',
+      'very-unhealthy',
+      'hazardous',
+    );
     donutChartIndicator.style.strokeDasharray =
       '115.19173063162564 172.78759594743863';
   } else if (airQualityIndex <= 300) {
     donutChartSVG.classList.add('very-unhealthy');
+    donutChartSVG.classList.remove(
+      'good',
+      'moderate',
+      'unhealthy-for-sensitive-groups',
+      'unhealthy',
+      'hazardous',
+    );
     donutChartIndicator.style.strokeDasharray =
       '144.9896632895322 172.78759594743863';
   } else {
     donutChartSVG.classList.add('hazardous');
+    donutChartSVG.classList.remove(
+      'good',
+      'moderate',
+      'unhealthy-for-sensitive-groups',
+      'unhealthy',
+      'very-unhealthy',
+    );
     donutChartIndicator.style.strokeDasharray =
       '172.78759594743863 172.78759594743863';
   }
